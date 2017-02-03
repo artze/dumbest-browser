8852d1399a234eb6c1f5202c76d2fbfbd4e918d3
artze <arthurwtm@gmail.com>
2017-02-03 12:25:54 +0800

    completed dumb browser

--- browser.rb
+++ browser.rb
@@ -1,7 +1,9 @@
 require 'net/http'
 require 'nokogiri'
+require 'uri'
+require 'open-uri'
 
-require_relative 'util'
+require_relative './page'
 
 class Browser
   def run!
@@ -14,6 +16,35 @@ class Browser
     # Questions:
     #  1. How can a user quit the browser gracefully?
     #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+    puts "Insert URL"
+    user_url = gets.chomp
+    new_page = Page.new(user_url)
+    new_page.fetch!
+
+    loop do 
+      puts 'What to extract?'
+      puts '1. Title'
+      puts '2. Links'
+      puts '3. Exit'
+
+      print 'Enter option number '
+      input = gets.chomp
+      puts ''
+
+      case input
+
+      when '1'
+        new_page.title
+      when '2'
+        new_page.links
+      when '3'
+        break
+      else
+        puts 'Input not recognized. Please try again.'
+        puts ''
+        next
+      end
+    end
   end
 end
 
--- page.rb
+++ page.rb
@@ -1,11 +1,22 @@
+
 class Page
   def initialize(url)
+    @url = url
   end
   
   def fetch!
+    print 'Fetching'
+    6.times do
+      sleep(0.2)
+      print '.'
+    end
+    puts ''
+    @html = Nokogiri::HTML(open(@url))
   end
   
   def title
+    puts 'Page Title: ' + @html.css('title').text
+    puts ''
   end
   
   def links
@@ -15,5 +26,10 @@ class Page
     # These should only be URLs that look like
     #   <a href="http://somesite.com/page.html">Click here!</a>
     # This would pull out "http://somesite.com/page.html"
+    puts 'All links in page:'
+    @html.css('a[href ^= http]').each do |link|
+      puts link['href']
+    end
+    puts ''
   end
 end
